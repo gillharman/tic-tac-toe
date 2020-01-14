42c6a8267faa289813ec5db0df3dac5d09d0ad6a
Harman Gill <gillhimmy@gmail.com>
2020-01-14 10:55:57 -0500

    Wrapping up - Challenge 3 complete

--- src/index.js
+++ src/index.js
@@ -22,45 +22,40 @@ class Board extends React.Component {
   }
 
   render() {
+    const number_of_squares = 9;
+    let square_count = 0;
+
+    let rows = [];
+
+    for(let i = 0; i < 3; i++) {
+      let columns = [];
+      for(let j = 0; j < 3; j++) {
+        if(square_count < number_of_squares) {
+          columns.push(this.renderSquare(square_count));
+          square_count++;
+        }
+      }
+      rows.push(columns);
+    }
+
+    var squares = rows.map((row) => {
+      return (
+        <div className="board-row">{row}</div>
+      );
+    });
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {squares}
       </div>
     );
   }
 }
 
-class Move extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: false,
-    };
-  }
-
-  handleClick() {
-    console.log("Hello World!");
-  }
-
-  render() {
-    return (
-      <button id={this.props.id} onClick={this.props.onClick}>{this.props.value}</button>
-    );
-  }
+function Move(props) {
+  return (
+    <button id={props.id} onClick={props.onClick}>{props.value}</button>
+  );
 }
 
 class Game extends React.Component {
